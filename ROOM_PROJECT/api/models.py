ac78bb66f1ee2016dce6cba20522df8ba6980374
MackKaputo <mackkaputo@gmail.com>
2021-03-20 22:14:14 +0200

    serializer set

--- ROOM_PROJECT/api/models.py
+++ ROOM_PROJECT/api/models.py
@@ -20,3 +20,6 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.code} with host: {self.host}"
